f51253f7185730adf764a91f522ad12c408f9c89
JoachimBos2001 <167633324+JoachimBos2001@users.noreply.github.com>
2024-04-20 19:10:38 +0200

    Update Arduino-LED-ON-AND-OFF.cpp
    You can use this code if you want to turn your LED on or off. Open the monitor and type: on to turn on the LED and off to turn off the LED.

--- Arduino-LED-ON-AND-OFF.cpp
+++ Arduino-LED-ON-AND-OFF.cpp
@@ -1,4 +1,4 @@
-const int ledPin 13; //* This is to tell where the LED is located.
+const int ledPin = 13; //* This is to tell where the LED is located.
 void setup() {
     pinMode(ledPin, OUTPUT);
     Serial.begin(9600);
@@ -8,7 +8,7 @@ void loop() {
         String input = Serial.readStringUntil('\n');
         if (input == "on") { //* You can change this .
             digitalWrite(ledPin, HIGH);
-            Serial.println("LED turned on") //* You can change this to if you want.
+            Serial.println("LED turned on"); //* You can change this to if you want.
         }
         else if (input == "off") { //* You can also change this.
             digitalWrite(ledPin, LOW);
